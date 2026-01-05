d0d6ff5e12eb088f170a86a382eb8c792b477f34
elkuku <der.el.kuku@gmail.com>
2026-01-05 10:34:46 -0500

    Addapt workflow to build dev and release versions

--- scripts/setup-github-page.js
+++ scripts/setup-github-page.js
@@ -13,17 +13,22 @@ fs.rmSync('gh_page', {recursive: true, force: true})
 fs.mkdirSync('gh_page', {recursive: true})
 
 fs.cpSync('github_page', 'gh_page', {recursive: true})
-fs.cpSync('dist', 'gh_page/files', {recursive: true})
+fs.cpSync('build', 'gh_page/files', {recursive: true})
 
-const fileNames = fs
-    .readdirSync('dist', {withFileTypes: true})
+const releaseFiles = fs
+    .readdirSync('build/release', {withFileTypes: true})
     .filter(entry => entry.isFile())
     .map(entry => entry.name)
 
-const metaFile = fileNames.filter(fileName => fileName.endsWith('.meta.js'))[0]
+const devFiles = fs
+    .readdirSync('build/dev', {withFileTypes: true})
+    .filter(entry => entry.isFile())
+    .map(entry => entry.name)
+
+const metaFile = releaseFiles.filter(fileName => fileName.endsWith('.meta.js'))[0]
 let version = 'n/a'
 if (metaFile) {
-    const meta = fs.readFileSync(`dist/${metaFile}`, 'utf8')
+    const meta = fs.readFileSync(`build/release/${metaFile}`, 'utf8')
     const match = meta.match(/^\s*\/\/\s*@version\s+(.+)$/m)
     version = match ? match[1].trim() : 'n/a'
 }
@@ -32,16 +37,22 @@ const pluginData = JSON.parse(
     fs.readFileSync('plugin.json', 'utf8'),
 )
 
-const links = fileNames
+const releaseLinks = releaseFiles
     .filter(name => !name.endsWith('meta.js'))
-    .map(name => `<li><a href="files/${name}">${name}</a></li>`)
+    .map(name => `<li><a href="files/release/${name}">${name}</a></li>`)
+    .join('\n')
+
+const devLinks = devFiles
+    .filter(name => !name.endsWith('meta.js'))
+    .map(name => `<li><a href="files/dev/${name}">${name}</a></li>`)
     .join('\n')
 
 let template = fs.readFileSync('gh_page/index.html', 'utf8')
 
 const projectName = pluginData.name.replace('IITC plugin: ', '')
 
-template = template.replace('{{FILE_LINKS}}', links)
+template = template.replace('{{DEV_LINKS}}', devLinks)
+template = template.replace('{{RELEASE_LINKS}}', releaseLinks)
 template = template.replaceAll('{{PROJECT_NAME}}', projectName)
 template = template.replaceAll('{{PROJECT_VERSION}}', version)
 template = template.replaceAll('{{LAST_UPDATED}}', formattedDate)
